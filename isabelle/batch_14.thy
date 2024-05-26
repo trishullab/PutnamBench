1c0b664c5a8e00a6b85028cb2c1bb61a386951ae
Jasper Lee <jasperlee@Jaspers-MBP-4.cable.rcn.com>
2024-05-26 11:24:06 -0400

    fixed previous Isabelle problem with cardinality of existential algebraic structure

--- isabelle/batch_14.thy
+++ isabelle/batch_14.thy
@@ -53,7 +53,8 @@ theorem putnam_2008_b5:
 definition putnam_2009_a5_solution :: bool where "putnam_2009_a5_solution \<equiv> undefined"
 (* False *)
 theorem putnam_2009_a5:
-  shows "(\<exists>G(structure). finite (carrier G) \<and> comm_group G \<and> (\<Prod>g\<in>(carrier G). (group.ord G) g) = 2^2009) \<longleftrightarrow> putnam_2009_a5_solution"
+  assumes pacount: "\<exists>pamap::'a\<Rightarrow>nat. surj pamap"
+  shows "(\<exists>G::'a monoid. finite (carrier G) \<and> comm_group G \<and> (\<Prod>g\<in>(carrier G). (group.ord G) g) = 2^2009) \<longleftrightarrow> putnam_2009_a5_solution"
   sorry
 
 (* uses (nat \<Rightarrow> nat) instead of (Fin k \<Rightarrow> nat) and (Fin m \<Rightarrow> nat) *)
