5401ad6def48f2ae84fb322f3f90720a45b46290
Jasper Lee <jasperlee@Jaspers-MBP-4.cable.rcn.com>
2024-05-26 01:02:30 -0400

    edited same previous isabelle formulation (real poly poly) to be clearer

--- isabelle/putnam_2005_b1.thy
+++ isabelle/putnam_2005_b1.thy
@@ -4,7 +4,7 @@ begin
 
 (* there might be multiple possible correct answers *)
 definition putnam_2005_b1_solution :: "real poly poly" where "putnam_2005_b1_solution \<equiv> undefined"
-(* (monom 1 1 - 2*(monom (monom 1 1) 0)) * (monom 1 1 - 2*(monom (monom 1 1) 0) - 1) *)
+(* (monom (monom 1 0) 1 - 2*(monom (monom 1 1) 0)) * (monom (monom 1 0) 1 - 2*(monom (monom 1 1) 0) - 1) *)
 theorem putnam_2005_b1: 
   shows "putnam_2005_b1_solution \<noteq> 0 \<and> (\<forall>a::real. poly (poly putnam_2005_b1_solution [:\<lfloor>2*a\<rfloor>:]) \<lfloor>a\<rfloor> = 0)"
   sorry
