0b3faef08c3a5103b7009fdecf95ea3469afaedb
Jasper Lee <jasperlee@Jaspers-MBP-4.cable.rcn.com>
2024-06-01 18:10:28 -0400

    did 1994-2003 lean existentials corrections

--- lean4/putnam_1994.lean
+++ lean4/putnam_1994.lean
@@ -28,7 +28,7 @@ theorem putnam_1994_a6
 (f : Fin 10 → Equiv.Perm ℤ)
 (mijcomp : ℕ → (ℕ → Fin 10) → ℕ → (ℤ → ℤ))
 (F : Finset (ℤ → ℤ))
-(hf: ∀ n : ℤ, ∃ m ≥ 1, ∃ i : ℕ → Fin 10, (mijcomp m i 0) 0 = n)
+(hf: ∀ n : ℤ, ∃ m : ℕ, ∃ i : ℕ → Fin 10, m ≥ 1 ∧ (mijcomp m i 0) 0 = n)
 (hmijcomp : ∀ m ≥ 1, ∀ (i : ℕ → Fin 10) (j : Fin m), mijcomp m i j = if (j = m - 1) then (f (i j) : ℤ → ℤ) else (f (i j) ∘ mijcomp m i (j + 1)))
 (hF : F = {g : ℤ → ℤ | ∃ e : Fin 10 → Fin 2, g = (f 0)^[e 0] ∘ (f 1)^[e 1] ∘ (f 2)^[e 2] ∘ (f 3)^[e 3] ∘ (f 4)^[e 4] ∘ (f 5)^[e 5] ∘ (f 6)^[e 6] ∘ (f 7)^[e 7] ∘ (f 8)^[e 8] ∘ (f 9)^[e 9]})
 : ∀ A : Finset ℤ, A.Nonempty → {g ∈ F | g '' A = A}.encard ≤ 512 :=
@@ -84,4 +84,3 @@ theorem putnam_1994_b6
 (hn : ∀ a : ℕ, n a = 101 * a - 100 * 2 ^ a)
 : ∀ a b c d : Set.Icc 0 99, (n a + n b ≡ n c + n d [ZMOD 10100]) → (({a, b} : Set (Set.Icc 0 99)) = {c, d}) :=
 sorry
-
