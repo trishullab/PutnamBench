c3919fb3913275cfa1e9e1194a35ca66b30dbf2e
Jasper Lee <jasperlee@Jaspers-MBP-4.cable.rcn.com>
2024-06-17 22:38:28 -0400

    added note to putnam_2012_a1 (acute triangle problem)

--- lean4/putnam_2012.lean
+++ lean4/putnam_2012.lean
@@ -3,6 +3,7 @@ open BigOperators
 
 open Matrix
 
+-- Note: This proof statement strays a bit from the problem statement since it manually checks a condition for acuteness based on side lengths.
 theorem putnam_2012_a1
 (d : Fin 12 → ℝ)
 (hd : ∀ i : Fin 12, d i ∈ Set.Ioo 1 12)
