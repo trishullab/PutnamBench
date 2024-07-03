daa21d5d91578cae3dd78166f240acf5ce112749
Jasper Lee <leejasper851@gmail.com>
2024-07-03 16:37:53 -0400

    changed 'inhabited' for nonempty fintypes

--- coq/batch_51.v
+++ coq/batch_51.v
@@ -25,7 +25,7 @@ Variables G B : finType.
 Theorem putnam_1965_a4
     (dances : G -> B -> Prop)
     (h : ~(exists b : B, forall g : G, dances g b) /\ (forall g : G, exists b : B, dances g b))
-    (nonempty : (exists g : G, True) /\ (exists b : B, True))
+    (nonempty : inhabited G /\ inhabited B)
     : exists (g h : G) (b c : B), dances g b /\ dances h c /\ ~dances h b /\ ~dances g c.
 Proof. Admitted.
 End putnam_1965_a4.
