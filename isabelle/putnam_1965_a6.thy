8627e5cf87fdf5cd04a7d5a301f15985b0a7cea4
Jasper Lee <leejasper851@gmail.com>
2024-07-02 21:50:55 -0400

    batch 51 isabelle fixes

--- isabelle/putnam_1965_a6.thy
+++ isabelle/putnam_1965_a6.thy
@@ -4,9 +4,11 @@ begin
 
 theorem putnam_1965_a6:
   fixes u v m :: real
+  and pinter :: "(real \<times> real) \<Rightarrow> bool"
   assumes hm: "m > 1"
   and huv: "u \<ge> 0 \<and> v \<ge> 0"
-  shows "(\<exists>! p :: real \<times> real. fst p \<ge> 0 \<and> snd p \<ge> 0 \<and> u * (fst p) + v * (snd p) = 1 \<and> (fst p) powr m + (snd p) powr m = 1) \<longleftrightarrow> (\<exists> n :: real. u powr n + v powr n = 1 \<and> m powi -1 + n powi -1 = 1)"
+  defines "pinter \<equiv> (\<lambda>p::real\<times>real. u * (fst p) + v * (snd p) = 1 \<and> (fst p) powr m + (snd p) powr m = 1)"
+  shows "((\<exists>! p :: real \<times> real. pinter p) \<and> (\<exists> p :: real \<times> real. fst p \<ge> 0 \<and> snd p \<ge> 0 \<and> pinter p)) \<longleftrightarrow> (\<exists> n :: real. u powr n + v powr n = 1 \<and> m powi -1 + n powi -1 = 1)"
   sorry
 
 end
