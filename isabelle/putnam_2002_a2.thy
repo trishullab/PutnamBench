ab9508f66c2306928de2fbac4c1ba0011a9021e4
Jasper Lee <leejasper851@gmail.com>
2024-07-11 22:50:43 -0400

    added isabelle problems from batch19982005hard

---
+++ isabelle/putnam_2002_a2.thy
@@ -0,0 +1,12 @@
+theory putnam_2002_a2 imports Complex_Main
+"HOL-Analysis.Finite_Cartesian_Product"
+"HOL-Analysis.Linear_Algebra"
+begin
+
+theorem putnam_2002_a2:
+  fixes hemi :: "(real^3) \<Rightarrow> ((real^3) set)"
+  defines "hemi \<equiv> (\<lambda>V::real^3. {P::real^3. P \<bullet> V \<ge> 0})"
+  shows "\<forall>S::(real^3) set. ((S \<subseteq> sphere 0 1 \<and> card S = 5) \<longrightarrow> (\<exists>V::real^3. V \<noteq> 0 \<and> card (S \<inter> hemi V) \<ge> 4))"
+  sorry
+
+end
