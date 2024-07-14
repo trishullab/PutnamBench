21079a4c6cd49a9578a89e36c7bdd497a1e9446a
Jasper Lee <leejasper851@gmail.com>
2024-07-14 01:12:52 -0400

    small adjustment

--- coq/src/putnam_1962_a6.v
+++ coq/src/putnam_1962_a6.v
@@ -1,8 +1,8 @@
 Require Import Ensembles QArith.
 Theorem putnam_1962_a6
     (SS : Ensemble Q)
-    (hSadd : forall a b : Q, (SS a /\ SS b) -> SS (a + b))
-    (hSprod : forall a b : Q, (SS a /\ SS b) -> SS (a * b))
-    (hScond : forall r : Q, (SS r \/ SS (-r) \/ r = 0) /\ ~(SS r \/ SS (-r)) /\ ~(SS r /\ r = 0) /\ ~(SS (-r) /\ r = 0))
+    (hSSadd : forall a b : Q, (SS a /\ SS b) -> SS (a + b))
+    (hSSprod : forall a b : Q, (SS a /\ SS b) -> SS (a * b))
+    (hSScond : forall r : Q, (SS r \/ SS (-r) \/ r = 0) /\ ~(SS r \/ SS (-r)) /\ ~(SS r /\ r = 0) /\ ~(SS (-r) /\ r = 0))
     : SS = (fun r : Q => r > 0).
 Proof. Admitted.
