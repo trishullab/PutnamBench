a21111ed39ab66effe87068cc325fa6b5e3c0f63
MichaelHenryJennings <michaeljennings@utexas.edu>
2024-07-23 22:56:19 -0500

    1992 Isabelle & Coq verifications

--- coq/src/putnam_1992_a2.v
+++ coq/src/putnam_1992_a2.v
@@ -3,5 +3,5 @@ Open Scope R.
 Definition putnam_1992_a2_solution := 1992.
 Theorem putnam_1992_a2
     (C : R -> R := fun a => (Derive_n (fun x => Rpower (1 + x) a) 1992) 0 / INR (fact 1992))
-    : RInt (fun y => C( - y - 1 ) * (sum_n (fun k => 1 / (y + INR k)) 1992)) 0 1 = putnam_1992_a2_solution.
+    : RInt (fun y => C (-y - 1) * sum_n_m (fun k => 1 / (y + INR k)) 1 1992) 0 1 = putnam_1992_a2_solution.
 Proof. Admitted.
--- coq/src/putnam_1992_b4.v
+++ coq/src/putnam_1992_b4.v
@@ -2,11 +2,17 @@ From mathcomp Require Import ssrnat ssrnum ssralg poly polydiv seq.
 Open Scope ring_scope.
 Definition putnam_1992_b4_solution := 3984%nat.
 Theorem putnam_1992_b4
-    (R : numDomainType) 
-    (p : {poly R})
-    (cond : {poly R} -> {poly R} -> Prop := fun f g => derivn 1992 (p %/ ('X^3 - 'X)) = f %/ g)
-    : gt (size p) 1992 /\ exists c: R, gcdp_rec p ('X^3 - 'X) = polyC c ->
-    exists mindeg, ((forall (f g: {poly R}), cond f g /\ ge (size f) mindeg) /\ 
-    (exists (f g: {poly R}), cond f g /\ size f = mindeg)) <->
-    mindeg = putnam_1992_b4_solution.
+    (R : numDomainType)
+    (itercomp := fix iter (f : {poly R} * {poly R} -> {poly R} * {poly R}) (n : nat) (p : {poly R} * {poly R}) : {poly R} * {poly R} :=
+        match n with
+        | O => p
+        | S n' => f (iter f n' p)
+    end)
+    (qr : {poly R} * {poly R} -> {poly R} * {poly R} := fun duple => (deriv (fst duple) * snd duple - deriv (snd duple) * fst duple, snd duple * snd duple))
+    (valid : {poly R} -> Prop := fun p => p <> 0 /\ lt (size p) 1992 /\ exists c : R, gcdp_rec p ('X^3 - 'X) = polyC c)
+    (twople : {poly R} -> {poly R} -> Prop := fun p f => exists g : {poly R}, g * fst (itercomp qr 1992%nat (p, 'X^3 - 'X)) = f * snd (itercomp qr 1992%nat (p, 'X^3 - 'X)))
+    (min : nat)
+    (hmineq : exists p f : {poly R}, (valid p /\ twople p f) /\ size f = min)
+    (hminlb : forall p f : {poly R}, (valid p /\ twople p f) -> ge (size f) min)
+    : min = putnam_1992_b4_solution.
 Proof. Admitted.
