c4c21d383fd9ca5abc8635516a85302fedcb035e
MichaelHenryJennings <michaeljennings@utexas.edu>
2024-07-24 16:24:19 -0500

    1993 Isabelle & Coq verifications

--- lean4/src/putnam_1993_b1.lean
+++ lean4/src/putnam_1993_b1.lean
@@ -5,6 +5,6 @@ abbrev putnam_1993_b1_solution : ℕ := sorry
 -- 3987
 theorem putnam_1993_b1
 (nallmexk : ℕ → Prop)
-(hnallmexk : ∀ n : ℕ, nallmexk n = (n > 0 ∧ ∀ m ∈ Set.Ioo 0 1993, ∃ k : ℤ, (m / 1993 < (k : ℝ) / n) ∧ ((k : ℝ) / n < (m + 1) / 1994)))
+(hnallmexk : ∀ n : ℕ, nallmexk n = (n > 0 ∧ ∀ m ∈ Set.Ioo (0 : ℤ) 1993, ∃ k : ℤ, (m / 1993 < (k : ℝ) / n) ∧ ((k : ℝ) / n < (m + 1) / 1994)))
 : nallmexk putnam_1993_b1_solution ∧ (∀ n : ℕ, nallmexk n → n ≥ putnam_1993_b1_solution) :=
 sorry
