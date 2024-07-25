48f7eacc5e0a9e65e6b2a31f987c09d970a9dc17
MichaelHenryJennings <michaeljennings@utexas.edu>
2024-07-25 02:23:50 -0500

    1995 Isabelle & Coq verifications

--- lean4/src/putnam_1995_a4.lean
+++ lean4/src/putnam_1995_a4.lean
@@ -6,7 +6,7 @@ open Filter Topology Real
 theorem putnam_1995_a4
 (n : ℕ)
 (hn : n > 0)
-(necklace : Fin n → ℤ)
-(hnecklacesum : ∑ i : Fin n, necklace i = n - 1)
-: ∃ cut : Fin n, ∀ k : Fin n, ∑ i : {j : Fin n | j.1 ≤ k}, necklace (cut + i) ≤ k :=
+(necklace : ℕ → ℤ)
+(hnecklacesum : ∑ i ∈ Finset.range n, necklace i = n - 1)
+: ∃ cut ∈ Finset.range n, ∀ k ∈ Finset.range n, ∑ i ≤ k, necklace ((cut + i) % n) ≤ k :=
 sorry
