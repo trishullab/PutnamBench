5c5faee726f9f8a8c62d859e398145d8bbbb605e
MichaelHenryJennings <michaeljennings@utexas.edu>
2024-07-27 14:55:17 -0500

    removed two Coq formalizations

--- coq/src/commented_problems.v
+++ coq/src/commented_problems.v
@@ -77,3 +77,34 @@ Theorem putnam_2010_a5:
                 cross_product a b = a. 
 Proof. Admitted.
 End putnam_2010_a5. *)
+
+(* From mathcomp Require Import matrix ssralg ssrbool.
+Open Scope ring_scope.
+Definition putnam_1991_a2_solution := False.
+Theorem putnam_1991_a2
+    (R : comUnitRingType) 
+    (n : nat) 
+    (npos : n >= 1)
+    : (exists A B : 'M[R]_n, A <> B /\ mulmx (mulmx A A) A = mulmx (mulmx B B) B /\
+    mulmx (mulmx A A) B = mulmx (mulmx B B) A /\
+    (mulmx A A + mulmx B B) \in unitmx) <-> putnam_1991_a2_solution.
+Proof. Admitted. *)
+
+(* From mathcomp Require Import ssrnat ssrnum ssralg poly polydiv seq.
+Open Scope ring_scope.
+Definition putnam_1992_b4_solution := 3984%nat.
+Theorem putnam_1992_b4
+    (R : numDomainType)
+    (itercomp := fix iter (f : {poly R} * {poly R} -> {poly R} * {poly R}) (n : nat) (p : {poly R} * {poly R}) : {poly R} * {poly R} :=
+        match n with
+        | O => p
+        | S n' => f (iter f n' p)
+    end)
+    (qr : {poly R} * {poly R} -> {poly R} * {poly R} := fun duple => (deriv (fst duple) * snd duple - deriv (snd duple) * fst duple, snd duple * snd duple))
+    (valid : {poly R} -> Prop := fun p => p <> 0 /\ lt (size p) 1992 /\ exists c : R, gcdp_rec p ('X^3 - 'X) = polyC c)
+    (twople : {poly R} -> {poly R} -> Prop := fun p f => exists g : {poly R}, g * fst (itercomp qr 1992%nat (p, 'X^3 - 'X)) = f * snd (itercomp qr 1992%nat (p, 'X^3 - 'X)))
+    (min : nat)
+    (hmineq : exists p f : {poly R}, (valid p /\ twople p f) /\ size f = min)
+    (hminlb : forall p f : {poly R}, (valid p /\ twople p f) -> ge (size f) min)
+    : min = putnam_1992_b4_solution.
+Proof. Admitted. *)
