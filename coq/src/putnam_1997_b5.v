731fac8b06ab46e5f931eab973af3e8eee8c4922
Jasper Lee <leejasper851@gmail.com>
2024-08-02 21:12:55 -0400

    changed 'powm' to 'tetration' in putnam 1997 b5

--- coq/src/putnam_1997_b5.v
+++ coq/src/putnam_1997_b5.v
@@ -1,11 +1,11 @@
 Require Import Nat.
 Theorem putnam_1997_b5
-    (powm := fix pow_m (b m: nat) : nat :=
+    (tetration := fix tetration' (b m: nat) : nat :=
       match m with
       | O => 1
-      | S m' => b ^ (pow_m b m')
+      | S m' => b ^ (tetration' b m')
     end)
     (n : nat)
     (hn : n >= 2)
-    : (powm 2 n) mod n = (powm 2 (n-1)) mod n.
+    : (tetration 2 n) mod n = (tetration 2 (n-1)) mod n.
 Proof. Admitted.
