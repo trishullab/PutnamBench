cf8d5d575feefe75fede144f5696f922ecbac83e
Eric Wieser <wieser.eric@gmail.com>
2024-08-12 12:39:07 +0000

    Update Lean and Mathlib to v4.10.0

--- lean4/lakefile.lean
+++ lean4/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 package «putnam» where
   -- add package configuration options here
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.9.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.10.0"
 
 @[default_target]
 lean_lib «putnam» where
