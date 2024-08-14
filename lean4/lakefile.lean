92e14b4ff5da40896b50f620588a20b2b43b2380
Eric Wieser <wieser.eric@gmail.com>
2024-08-14 10:49:23 +0100

    Disable autoImplicits
    These are a common source of misformalizations.

--- lean4/lakefile.lean
+++ lean4/lakefile.lean
@@ -2,6 +2,9 @@ import Lake
 open Lake DSL
 
 package «putnam» where
+  leanOptions := #[
+    ⟨`autoImplicit, false⟩
+  ]
   -- add package configuration options here
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.7.0"
 
