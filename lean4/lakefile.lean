9a9436d828a28b4c27a80500633dddceca2b1d0a
MichaelHenryJennings <michaeljennings@utexas.edu>
2024-08-17 17:25:10 -0500

    Disabled autoimplicits (for CI checks)

--- lean4/lakefile.lean
+++ lean4/lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package «putnam» where
   -- add package configuration options here
+  leanOptions := #[
+    ⟨`autoImplicit, false⟩
+  ]
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.10.0"
 
 @[default_target]
