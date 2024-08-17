64e63b68cd1804179341d0283b0eadabaf259d56
Oliver Nash <ddojsioc@google.com>
2024-08-17 17:39:00 +0000

    Remove solution from statement of `putnam_2021_b2`
    It was accidentally included in 3e4fc8633fb036859e13828a8646d4f7f3f3a146

--- lean4/src/putnam_2021_b2.lean
+++ lean4/src/putnam_2021_b2.lean
@@ -9,5 +9,5 @@ theorem putnam_2021_b2 :
     IsGreatest
       {S | ∃ a : ℕ+ → ℝ, (∑' k, a k = 1) ∧ (∀ k, 0 ≤ a k) ∧
         S = ∑' n : ℕ+, n / 2 ^ (n : ℕ) * (∏ k in Finset.Icc 1 n, a k) ^ (1 / n : ℝ)}
-      (2 / 3) :=
+      putnam_2021_b2_solution :=
   sorry
