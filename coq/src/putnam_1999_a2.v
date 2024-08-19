1acfb55b249598671d36d10e938a8399562389a3
Jasper Lee <leejasper851@gmail.com>
2024-08-19 00:39:13 -0500

    fixed putnam 1999 a2 using mathcomp.analysis (added to setup.sh)

--- coq/src/putnam_1999_a2.v
+++ coq/src/putnam_1999_a2.v
@@ -1,7 +1,7 @@
-From mathcomp Require Import ssrnat ssrnum ssralg fintype poly.
+From mathcomp.analysis Require Import reals. From mathcomp Require Import ssrnat ssrnum ssralg fintype poly.
 Open Scope ring_scope.
 Theorem putnam_1999_a2
-    (R: numDomainType)
+    (R : realType)
     (p : {poly R})
     (hpos : forall x, (p.[x] >= 0) = true)
     : exists (k : nat) (f : nat -> {poly R}), p = \sum_(i < k) ((f i)*(f i)).
