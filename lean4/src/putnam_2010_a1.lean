42b6b86ff50f2b959e869f453499986a78c9d457
Oliver Nash <ddojsioc@google.com>
2024-08-22 14:06:03 +0000

    Fix 2010 A1
    The original statement was false because it contained an off-by-one
    error. Specificially it concerned the numbers 0, 1, ..., n - 1 instead
    of the numbers 1, 2, ..., n. Using `Finset.Icc 1 n` instead of `Fin n`
    fixes this.
    
    Two other changes are made as points of style:
     * We do not use the topological sum `tsum` (with notation `∑'`) since
       we have finite sums.
     * We use `IsGreatest` to state the goal

--- lean4/src/putnam_2010_a1.lean
+++ lean4/src/putnam_2010_a1.lean
@@ -4,9 +4,12 @@ open BigOperators
 noncomputable abbrev putnam_2010_a1_solution : ℕ → ℕ := sorry
 -- (fun n : ℕ => Nat.ceil ((n : ℝ) / 2))
 theorem putnam_2010_a1
-(n : ℕ)
-(kboxes : ℕ → Prop)
-(npos : n > 0)
-(hkboxes : ∀ k : ℕ, kboxes k = (∃ boxes : Fin n → Fin k, ∀ i j : Fin k, (∑' x : boxes ⁻¹' {i}, (x : ℕ)) = (∑' x : boxes ⁻¹' {j}, (x : ℕ))))
-: kboxes (putnam_2010_a1_solution n) ∧ (∀ k : ℕ, kboxes k → k ≤ putnam_2010_a1_solution n) :=
-sorry
+    (n : ℕ)
+    (kboxes : ℕ → Prop)
+    (npos : n > 0)
+    (hkboxes : ∀ k : ℕ, kboxes k =
+      (∃ boxes : Finset.Icc 1 n → Fin k, ∀ i j : Fin k,
+        ∑ x in Finset.univ.filter (boxes · = i), (x : ℕ) =
+        ∑ x in Finset.univ.filter (boxes · = j), (x : ℕ))) :
+    IsGreatest kboxes (putnam_2010_a1_solution n) :=
+  sorry
