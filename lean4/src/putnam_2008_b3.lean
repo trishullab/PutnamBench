1bae7086b5f5cd3e1b65b3e32df3fdb4381635da
Oliver Nash <ddojsioc@google.com>
2024-08-29 13:52:21 +0000

    Fix 2008 B3
    The previous statement was disprovable because it did not demand `P` and
    `Q` appearing in `contains` be linearly independent. For example, the
    following was provable using it:
    ```
    have (r : ℝ) (O) : contains r O 0 0 := by simp [hcontains]
    ```
    
    I have opted to fix this with a rewrite rather than a patch.

--- lean4/src/putnam_2008_b3.lean
+++ lean4/src/putnam_2008_b3.lean
@@ -1,14 +1,17 @@
 import Mathlib
 open BigOperators
 
-open Filter Topology Set Nat
+open FiniteDimensional Metric Filter Topology Set Nat
 
 noncomputable abbrev putnam_2008_b3_solution : ℝ := sorry
 -- Real.sqrt 2 / 2
 theorem putnam_2008_b3
-(hypercube : Set (EuclideanSpace ℝ (Fin 4)))
-(hhypercube : hypercube = {P : Fin 4 → ℝ | ∀ i : Fin 4, |P i| ≤ 1 / 2})
-(contains : ℝ → (EuclideanSpace ℝ (Fin 4)) → (EuclideanSpace ℝ (Fin 4)) → (EuclideanSpace ℝ (Fin 4)) → Prop)
-(hcontains : contains = fun r O P Q ↦ ∀ s t : ℝ, (s • P + t • Q ≠ 0 ∧ dist 0 (s • P + t • Q) = r) → O + s • P + t • Q ∈ hypercube)
-: (∃ O P Q, contains putnam_2008_b3_solution O P Q) ∧ (∀ r > putnam_2008_b3_solution, ¬∃ O P Q, contains r O P Q) :=
-sorry
+    (H : Set (EuclideanSpace ℝ (Fin 4)))
+    (H_def : H = {P : Fin 4 → ℝ | ∀ i : Fin 4, |P i| ≤ 1 / 2})
+    (contains : ℝ → Prop)
+    (contains_def : ∀ r, contains r ↔
+      ∃ᵉ (A : AffineSubspace ℝ (EuclideanSpace ℝ (Fin 4))) (C ∈ A),
+        finrank A.direction = 2 ∧
+        sphere C r ∩ A ⊆ H) :
+    IsGreatest contains putnam_2008_b3_solution :=
+  sorry
