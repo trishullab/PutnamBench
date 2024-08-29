3f98c26d319a6780d77600616b15b8ea906f5ef8
Oliver Nash <ddojsioc@google.com>
2024-08-29 14:25:56 +0000

    Fix 2003 B3
    The old statement was disprovable because it did not sufficiently
    characterise the `lcmicc` function. For example the zero function
    satisfies the predicate `hlcmicc`.
    
    Mathlib should really contain an LCM function for finite sets but since
    it does not exist, we have inlined a version of one in a rewrite here.
    
    We have also dropped the requirement that `n` be strictly positive since
    the theorem is in fact still true for `n = 0`.

--- lean4/src/putnam_2003_b3.lean
+++ lean4/src/putnam_2003_b3.lean
@@ -3,10 +3,6 @@ open BigOperators
 
 open MvPolynomial Set Nat
 
-theorem putnam_2003_b3
-(multicc : ℕ → ℕ → Prop)
-(lcmicc : ℕ → ℕ)
-(hmulticc : ∀ n mult : ℕ, multicc n mult = ∀ i : Set.Icc 1 n, i.1 ∣ mult)
-(hlcmicc : ∀ n : ℕ, multicc n (lcmicc n) ∧ (∀ n' : ℕ, multicc n n' → n' ≥ (lcmicc n)))
-: ∀ n > 0, (n)! = ∏ i : Set.Icc 1 n, lcmicc (n / i.1) :=
-sorry
+theorem putnam_2003_b3 (n : ℕ) :
+    n ! = ∏ i in Finset.Icc 1 n, ((List.range ⌊n / i⌋₊).map succ).foldl Nat.lcm 1 := by
+  sorry
