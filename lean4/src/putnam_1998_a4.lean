a493028f5ef61521f0b7d5bb50f475292af333eb
Oliver Nash <ddojsioc@google.com>
2024-08-29 15:16:39 +0000

    Fix 1998 A4
    The was disprovable because `Nat.digits 10 0` is `[]` rather than `[0]`.
    In fact there is no reason to go back and forth with digits in the
    definition of `A` so we rewrite to avoid this. We also avoid Nat
    subtraction by expressing the recurrence using `n + 2` instead of `n`.
    
    We note that the `List.reverse` is optional since 11 divides a number
    iff it divides its base-10 reverse. However it seems more faithful to
    the question not to embed this fact in the statement.

--- lean4/src/putnam_1998_a4.lean
+++ lean4/src/putnam_1998_a4.lean
@@ -1,13 +1,13 @@
 import Mathlib
 open BigOperators
 
--- Note: Since .digits and .ofDigits work in little-endian mode, one must switch the order of the concatenation here.
+-- Note: Since 11 divides `x` iff it divides its base-10 reverse, the `reverse` below is optional.
 abbrev putnam_1998_a4_solution : Set ℕ := sorry
 -- {n | n ≡ 1 [MOD 6]}
 theorem putnam_1998_a4
-(A : ℕ → ℕ)
-(hA1 : A 1 = 0)
-(hA2 : A 2 = 1)
-(hA : ∀ n : ℕ, n > 2 → A n = Nat.ofDigits 10 (Nat.digits 10 (A (n - 2)) ++ Nat.digits 10 (A (n - 1))))
-: putnam_1998_a4_solution = {n | n ≥ 1 ∧ (11 ∣ A n)} :=
-sorry
+    (A : ℕ → List ℕ)
+    (hA1 : A 1 = [0])
+    (hA2 : A 2 = [1])
+    (hA : ∀ n, A (n + 2) = A (n + 1) ++ A n) :
+    {n | 1 ≤ n ∧ 11 ∣ Nat.ofDigits 10 (A n).reverse} = putnam_1998_a4_solution :=
+  sorry
