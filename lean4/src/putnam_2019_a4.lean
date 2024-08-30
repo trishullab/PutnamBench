55dc2ff8d3a390a2a9dad4df155babc51840d7d6
Oliver Nash <ddojsioc@google.com>
2024-08-30 17:41:30 +0000

    Use simpler expression in 2019 A4

--- lean4/src/putnam_2019_a4.lean
+++ lean4/src/putnam_2019_a4.lean
@@ -7,6 +7,6 @@ abbrev putnam_2019_a4_solution : Prop := sorry
 -- False
 theorem putnam_2019_a4
     (P : (EuclideanSpace ℝ (Fin 3) → ℝ) → Prop)
-    (P_def : ∀ f, P f ↔ ∀ C, integral μH[2] ((sphere C 1).restrict f) = 0) :
+    (P_def : ∀ f, P f ↔ ∀ C, ∫ x in sphere C 1, f x ∂μH[2] = 0) :
     (∀ f, Continuous f → P f → f = 0) ↔ putnam_2019_a4_solution :=
   sorry
