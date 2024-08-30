5a708c7d61403fe7fd4ada701bb4c31be14e1b7d
Oliver Nash <ddojsioc@google.com>
2024-08-30 11:44:02 +0000

    Fix 2019 A4
    This was accidentally trivial because the key condition was integrating
    a function over a 2-dimensional surface using a 3-dimensional measure
    (which is always zero) and thus was an empty condition.
    
    Unfortunately we encounter a gap in Mathlib here (no Riemannian
    manifolds) and so we are forced to use the Hausdorff measure instead of
    what one would usually see (the measure on the sphere obtained from a
    volume form it has as a Riemannian submanifold of Euclidean space).

--- lean4/src/putnam_2019_a4.lean
+++ lean4/src/putnam_2019_a4.lean
@@ -1,12 +1,12 @@
 import Mathlib
 open BigOperators
 
-open Topology Filter
+open MeasureTheory Metric Topology Filter
 
 abbrev putnam_2019_a4_solution : Prop := sorry
 -- False
 theorem putnam_2019_a4
-(fint : ((EuclideanSpace ℝ (Fin 3)) → ℝ) → Prop)
-(hfint : ∀ f : (EuclideanSpace ℝ (Fin 3)) → ℝ, fint f = (∀ S : EuclideanSpace ℝ (Fin 3), (∫ x in {p : EuclideanSpace ℝ (Fin 3) | dist p S = 1}, f x) = 0))
-: (∀ f : (EuclideanSpace ℝ (Fin 3)) → ℝ, (Continuous f ∧ fint f) → (∀ x : EuclideanSpace ℝ (Fin 3), f x = 0)) ↔ putnam_2019_a4_solution :=
-sorry
+    (P : (EuclideanSpace ℝ (Fin 3) → ℝ) → Prop)
+    (P_def : ∀ f, P f ↔ ∀ C, integral μH[2] ((sphere C 1).restrict f) = 0) :
+    (∀ f, Continuous f → P f → f = 0) ↔ putnam_2019_a4_solution :=
+  sorry
