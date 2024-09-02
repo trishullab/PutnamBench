b940dc5a3ed1adb5aafd840de4b2ab0b45052b21
Oliver Nash <ddojsioc@google.com>
2024-09-02 14:22:07 +0000

    Fix 1990 B1
    This was disprovable because `Set.Ioo 0 x` is empty when `x` is negative
    (and thus the integral is zero). The right tool for such integrals is
    Mathlib's `interval_integral` which uses the `a..b` notation.
    
    I have made this fix together with some small style tweaks.

--- lean4/src/putnam_1990_b1.lean
+++ lean4/src/putnam_1990_b1.lean
@@ -6,8 +6,9 @@ open Filter Topology Nat
 abbrev putnam_1990_b1_solution : Set (ℝ → ℝ) := sorry
 -- {fun x : ℝ => (Real.sqrt 1990) * Real.exp x, fun x : ℝ => -(Real.sqrt 1990) * Real.exp x}
 theorem putnam_1990_b1
-(f : ℝ → ℝ)
-(fint : Prop)
-(hfint : fint = ∀ x : ℝ, (f x) ^ 2 = (∫ t in Set.Ioo 0 x, (f t) ^ 2 + (deriv f t) ^ 2) + 1990)
-: (ContDiff ℝ 1 f ∧ fint) ↔ f ∈ putnam_1990_b1_solution :=
-sorry
+    (P : (ℝ → ℝ) → Prop)
+    (P_def : ∀ f, P f ↔ ∀ x,
+      (f x) ^ 2 = (∫ t in (0 : ℝ)..x, (f t) ^ 2 + (deriv f t) ^ 2) + 1990)
+    (f : ℝ → ℝ) :
+    (ContDiff ℝ 1 f ∧ P f) ↔ f ∈ putnam_1990_b1_solution :=
+  sorry
