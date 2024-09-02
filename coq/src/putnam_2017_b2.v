f07efd91dd85841b13a18902e6137d2730c10b91
George Tsoukalas <george.tsoukalas@utexas.edu>
2024-09-02 15:43:49 +0000

    Modify wording.

--- coq/src/putnam_2017_b2.v
+++ coq/src/putnam_2017_b2.v
@@ -1,12 +1,14 @@
-Require Import Nat ZArith Coquelicot.Coquelicot.
-Definition putnam_2017_b2_solution := 16.
-Theorem putnam_2017_b2
-    (mina : nat)
-    (posMin : mina > 0)
-    (A : nat -> nat -> nat := fun a k => Z.to_nat (floor (sum_n (fun i => Raxioms.INR (a + (i + 1))) k)))
-    (p : nat -> nat -> Prop := fun N k => exists (a: nat), a > 0 /\ A a k = N) 
-    (q : nat -> Prop := fun N => p N 2017 /\ forall (k: nat), k > 1 -> k <> 2017 -> ~ p N k)
-    (hmina : q (A mina 2017)) 
-    (hminalb : (forall (a: nat), a > 0 /\ q (A a 2017) -> mina <= a))
-    : mina = putnam_2017_b2_solution.
+From mathcomp Require Import all_ssreflect all_algebra.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Local Open Scope ring_scope.
+
+Definition putnam_2017_b2_solution : nat := 16.
+Theorem putnam_2017_b2 :
+    let seq (a : int) (k : nat) := \sum_(0 <= i < k) (a + i%:Z) in
+    let valid (a : int) := a > 0 /\ (forall (b : int) (k : nat), b > 0 -> gt k 1 -> seq a 2017%nat = seq b k -> k = 2017%nat) in
+    valid putnam_2017_b2_solution /\ (forall a, valid a -> a >= putnam_2017_b2_solution%:Z).
 Proof. Admitted.
