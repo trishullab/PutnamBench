46892171be0aacac8dc148b5800235744321f3da
Oliver Nash <ddojsioc@google.com>
2024-09-03 15:41:22 +0000

    Fix 1974 B1
    This was disprovable because the distance `B` appearing in
    `putnam_1974_b1_solution` was allowed to be zero, meaning that the same
    point repeated five times was incorrectly counted as a solution. The fix
    is to demand `B > 0`. Other changes are just style improvements.

--- lean4/src/putnam_1974_b1.lean
+++ lean4/src/putnam_1974_b1.lean
@@ -3,12 +3,13 @@ open BigOperators
 
 open Set Nat Polynomial
 
-abbrev putnam_1974_b1_solution : (Fin 5 → (ℝ × ℝ)) -> Prop := sorry
--- fun points => (∃ (B : ℝ) (ordering : Equiv.Perm (Fin 5)), ∀ i : Fin 5, Real.sqrt (((points (ordering i)).1 - (points (ordering (i+1))).1)^2 + ((points (ordering i)).2 - (points (ordering (i+1))).2)^2) = B)
+abbrev putnam_1974_b1_solution : (Fin 5 → EuclideanSpace ℝ (Fin 2)) → Prop := sorry
+-- fun p ↦ ∃ᵉ (B > 0) (o : Equiv.Perm (Fin 5)), ∀ i, dist (p (o i)) (p (o (i + 1))) = B
+
 theorem putnam_1974_b1
-(on_unit_circle : (Fin 5 → (ℝ × ℝ)) -> Prop)
-(distance_fun : (Fin 5 → (ℝ × ℝ)) -> ℝ)
-(h_on_unit_circle : on_unit_circle = fun points => ∀ i : Fin 5, Real.sqrt ((points i).1^2 + (points i).2^2) = 1)
-(hdistance_fun : distance_fun = fun points => ∑ idx : Fin 5 × Fin 5, if idx.1 < idx.2 then Real.sqrt (((points idx.1).1 - (points idx.2).1)^2 + ((points idx.1).2 - (points idx.2).2)^2) else 0)
-: ∀ points : Fin 5 → (ℝ × ℝ), on_unit_circle points → (distance_fun points = sSup {R | ∃ pts, on_unit_circle pts ∧ R = distance_fun pts} ↔ putnam_1974_b1_solution points) :=
-sorry
+    (d : (Fin 5 → EuclideanSpace ℝ (Fin 2)) → ℝ)
+    (d_def : ∀ p, d p = ∑ ⟨i, j⟩ : Fin 5 × Fin 5, if i < j then dist (p i) (p j) else 0)
+    (p : Fin 5 → EuclideanSpace ℝ (Fin 2))
+    (hp : ∀ i, ‖p i‖ = 1) :
+    d p = sSup {d q | (q) (hq : ∀ i, ‖q i‖ = 1)} ↔ putnam_1974_b1_solution p :=
+  sorry
