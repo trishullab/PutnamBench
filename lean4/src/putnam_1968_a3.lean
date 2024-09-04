f1d6d78a29c55f0b54c1e8fa341c0745b18c1653
Oliver Nash <ddojsioc@google.com>
2024-09-04 14:25:49 +0000

    Fix 1968 A3
    This was disprovable because the argument `ha : SDiff (Finset a)`
    allowed the user to fill in any function for the set difference
    function. So for example by providing a dummy set difference function
    that just always returned the empty set, the goal was false.
    
    Presumably this error arose because the original formaliser found that
    they could not use the existing set difference function out of the box.
    The right fix is either to put `open Classical` before the proof or to
    add the typeclass argument `[DecidableEq a]`.
    
    In the end I have provided rewrite since the original formalisation is
    a bit more complicated than it needs to be.

--- lean4/src/putnam_1968_a3.lean
+++ lean4/src/putnam_1968_a3.lean
@@ -1,12 +1,12 @@
 import Mathlib
 open BigOperators
 
-open Finset
+open Finset symmDiff
 
 theorem putnam_1968_a3
-{a : Type}
-(S : Finset a)
-(ha : SDiff (Finset a))
-: ∃ l : List (Finset a), (∀ i : Fin l.length, l[i] ⊆ S) ∧ l[0]! = ∅ ∧ (∀ s ⊆ S, ∃! i : Fin l.length, l[i] = s) ∧
-∀ i ∈ Finset.range (l.length - 1), (l[i]! ⊆ l[i+1]! ∧ (l[i+1]! \ l[i]!).card = 1) ∨ (l[i+1]! ⊆ l[i]! ∧ (l[i]! \ l[i+1]!).card = 1) :=
-sorry
+    (α : Type*) [Finite α] :
+    ∃ (n : ℕ) (s : Fin (2 ^ n) → Set α),
+      s 0 = ∅ ∧
+      (∀ t, ∃! i, s i = t) ∧
+      (∀ i, i + 1 < 2 ^ n → (s i ∆ s (i + 1)).ncard = 1) := by
+  sorry
