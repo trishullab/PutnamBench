12c5340e56cfe6c8e73e7a22a56d23b18e04a7df
Oliver Nash <ddojsioc@google.com>
2024-09-04 17:50:46 +0000

    Fix 2018 B5 (again)
    This was previously "fixed" in c75c0b07dd1b760a15a1f45aa0f14a30eb69b189
    but unfortunately an error remained.

--- lean4/src/putnam_2018_b5.lean
+++ lean4/src/putnam_2018_b5.lean
@@ -6,8 +6,8 @@ open Function
 theorem putnam_2018_b5
     (f : (Fin 2 → ℝ) → (Fin 2 → ℝ))
     (h₁ : ContDiff ℝ 1 f)
-    (h₂ : ∀ x i j, 0 < fderiv ℝ f x i j)
-    (h₃ : ∀ x, 0 < fderiv ℝ f x 0 0 * fderiv ℝ f x 1 1 -
-      (1 / 4) * (fderiv ℝ f x 0 1 + fderiv ℝ f x 1 0) ^ 2) :
+    (h₂ : ∀ x i j, 0 < fderiv ℝ f x (Pi.single i 1) j)
+    (h₃ : ∀ x, 0 < fderiv ℝ f x ![1, 0] 0 * fderiv ℝ f x ![0, 1] 1 -
+      (1 / 4) * (fderiv ℝ f x ![1, 0] 1 + fderiv ℝ f x ![0, 1] 0) ^ 2) :
     Injective f :=
   sorry
