69ca70d4e15dc7feb92d83f23815063ceaec83e2
Oliver Nash <ddojsioc@google.com>
2024-09-04 13:38:22 +0000

    Fix 1969 A5
    The original form was disprovable because of quantifier confusion (e.g.,
    taking `x` and `y` to be non-zero and constant gave a contradiction).

--- lean4/src/putnam_1969_a5.lean
+++ lean4/src/putnam_1969_a5.lean
@@ -4,7 +4,16 @@ open BigOperators
 open Matrix Filter Topology Set Nat
 
 theorem putnam_1969_a5
-: ∀ x y : ℝ → ℝ, (Differentiable ℝ x ∧ Differentiable ℝ y) → ∀ t > 0,
-(x 0 = y 0 ↔ ∃ u : ℝ → ℝ, Continuous u ∧ x t = 0 ∧ y t = 0 ∧
-deriv x = (fun _ : ℝ ↦ -2) * y + u ∧ deriv y = (fun _ : ℝ ↦ -2) * x + u) :=
-sorry
+    (x0 y0 t : ℝ)
+    (ht : 0 < t) :
+    x0 = y0 ↔ ∃ x y u : ℝ → ℝ,
+      Differentiable ℝ x ∧
+      Differentiable ℝ y ∧
+      Continuous u ∧
+      deriv x = - 2 • y + u ∧
+      deriv y = - 2 • x + u ∧
+      x 0 = x0 ∧
+      y 0 = y0 ∧
+      x t = 0 ∧
+      y t = 0 := by
+  sorry
