8c3dbc1bffb9d8db81a78b2249c32387515f72cf
Oliver Nash <ddojsioc@google.com>
2024-09-04 11:16:49 +0000

    Fix 1969 B2
    This was disprovable because the key predicate did not quantify over all
    groups (and the goal was false even for the trivial group).

--- lean4/src/putnam_1969_b2.lean
+++ lean4/src/putnam_1969_b2.lean
@@ -6,9 +6,8 @@ open Matrix Filter Topology Set Nat
 abbrev putnam_1969_b2_solution : Prop := sorry
 -- False
 theorem putnam_1969_b2
-(G : Type*)
-[Group G] [Finite G]
-(h : ℕ → Prop)
-(h_def : h = fun n => ∃ H : Fin n → Subgroup G, (∀ i : Fin n, (H i) < ⊤) ∧ ((⊤ : Set G) = ⋃ i : Fin n, (H i)))
-: ¬(h 2) ∧ ((¬(h 3)) ↔ putnam_1969_b2_solution) :=
-sorry
+    (P : ℕ → Prop)
+    (P_def : ∀ n, P n ↔ ∀ (G : Type) [Group G] [Finite G],
+      ∀ H : Fin n → Subgroup G, (∀ i, H i < ⊤) → ⋃ i, (H i : Set G) < ⊤) :
+    P 2 ∧ (P 3 ↔ putnam_1969_b2_solution) :=
+  sorry
