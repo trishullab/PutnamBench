8b11ed6683312084c6d305031620fbd801838f1c
Oliver Nash <ddojsioc@google.com>
2024-09-13 13:55:12 +0000

    Fix 2007 A1
    This was disprovable because of division-by-zero issues (`a = 0` was a
    solution).

--- lean4/src/putnam_2007_a1.lean
+++ lean4/src/putnam_2007_a1.lean
@@ -1,10 +1,12 @@
 import Mathlib
 open BigOperators
 
--- Note: Modified definition of tangent to handle this problem
 abbrev putnam_2007_a1_solution : Set ℝ := sorry
--- {2/3, 3/2, (13 + Real.sqrt 601)/12, (13 - Real.sqrt 601)/12}
-def reflect_tangent (f g : ℝ → ℝ) := ContDiff ℝ 1 f ∧ ContDiff ℝ 1 g ∧ (∃ x y : ℝ, f x = y ∧ g y = x ∧ (deriv f) x = 1 / (deriv g) y)
+-- {2 / 3, 3 / 2, (13 + √601) / 12, (13 - √601) / 12}
+
 theorem putnam_2007_a1
-: ∀ a : ℝ, reflect_tangent (fun x => a * x^2 + a * x + 1/24) (fun y => a * y^2 + a * y + 1/24) ↔ a ∈ putnam_2007_a1_solution :=
-sorry
+    (P : (ℝ → ℝ) → Prop)
+    (P_def : ∀ f, P f ↔ ∃ x y, f x = y ∧ f y = x ∧ deriv f x * deriv f y = 1)
+    (α : ℝ) :
+    P (fun t ↦ α * t ^ 2 + α * t + 1 / 24) ↔ α ∈ putnam_2007_a1_solution :=
+  sorry
