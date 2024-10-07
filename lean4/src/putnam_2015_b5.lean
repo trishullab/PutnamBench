bc4f5d7a51dabdd2478dfcd874065c1671145dda
George Tsoukalas <george.tsoukalas@utexas.edu>
2024-10-07 15:36:04 +0000

    Add minor notational improvements.

--- lean4/src/putnam_2015_b5.lean
+++ lean4/src/putnam_2015_b5.lean
@@ -8,5 +8,6 @@ abbrev putnam_2015_b5_solution : ℕ := sorry
 theorem putnam_2015_b5
 (P : ℕ → ℕ)
 (hP : P = fun n ↦ {pi : Finset.Icc 1 n → Finset.Icc 1 n | Bijective pi ∧ ∀ i j : Finset.Icc 1 n, Nat.dist i j = 1 → Nat.dist (pi i) (pi j) ≤ 2}.ncard)
-: (∀ n : ℕ, n ≥ 2 →  (P (n + 5) : ℤ) - (P (n + 4) : ℤ) - (P (n + 3) : ℤ) + (P n : ℤ) = putnam_2015_b5_solution) :=
+: (∀ n : ℕ, n ≥ 2 →
+    (P (n + 5) : ℤ) - (P (n + 4) : ℤ) - (P (n + 3) : ℤ) + (P n : ℤ) = putnam_2015_b5_solution) :=
 sorry
--- lean4/src/putnam_2021_a6.lean
+++ lean4/src/putnam_2021_a6.lean
@@ -6,9 +6,10 @@ open Filter Topology
 abbrev putnam_2021_a6_solution : Prop := sorry
 -- True
 theorem putnam_2021_a6
-(Pcoeff : Polynomial ℤ → Prop)
-(Pprod : Polynomial ℤ → Prop)
-(hPcoeff : ∀ P : Polynomial ℤ, Pcoeff P = (∀ n : ℕ, P.coeff n = 0 ∨ P.coeff n = 1))
-(hPprod : ∀ P : Polynomial ℤ, Pprod P = (∃ Q R : Polynomial ℤ, Q.degree > 0 ∧ R.degree > 0 ∧ P = Q * R))
-: (∀ P : Polynomial ℤ, (Pcoeff P ∧ Pprod P) → (P.eval 2 ≠ 0 ∧ P.eval 2 ≠ 1 ∧ ¬Prime (P.eval 2))) ↔ putnam_2021_a6_solution :=
+(Pcoeff Pprod : Polynomial ℤ → Prop)
+(hPcoeff : ∀ P, Pcoeff P = (∀ n : ℕ, P.coeff n = 0 ∨ P.coeff n = 1))
+(hPprod : ∀ P, Pprod P = (∃ Q R : Polynomial ℤ, Q.degree > 0 ∧ R.degree > 0 ∧ P = Q * R))
+: (∀ P, (Pcoeff P ∧ Pprod P)
+    → (P.eval 2 ≠ 0 ∧ P.eval 2 ≠ 1 ∧ ¬Prime (P.eval 2)))
+  ↔ putnam_2021_a6_solution :=
 sorry
