e653e8146e05a6009c25ffdbd071b09746a92d53
George Tsoukalas <george.tsoukalas@utexas.edu>
2024-10-13 01:49:37 +0000

    Remove commented old formalization.

--- coq/src/putnam_2002_a1.v
+++ coq/src/putnam_2002_a1.v
@@ -1,14 +1,3 @@
-(* Require Import Reals Factorial Coquelicot.Coquelicot.
-Definition putnam_2002_a1_solution (k n: nat) := Rpower (-1 * INR k) (INR n) * INR (fact n).
-Theorem putnam_2002_a1
-    (k : nat)
-    (p : (nat -> R) -> R -> nat -> R := fun a x n => sum_n (fun i => a i * x ^ i) n)
-    (kpos : gt k 0)
-    : forall (N: nat), forall (a: nat -> R) (n: nat),
-    (forall (x: R), (Derive_n (fun x => 1 / (x ^ k - 1)) N) x = (p a x n) / (x ^ k - 1) ^ (n + 1)) ->
-    p a 1 n = putnam_2002_a1_solution k n.
-Proof. Admitted. *)
-
 From mathcomp Require Import all_algebra all_ssreflect.
 From mathcomp Require Import reals normedtype sequences topology derive.
 From mathcomp Require Import classical_sets.
--- coq/src/putnam_2023_a1.v
+++ coq/src/putnam_2023_a1.v
@@ -1,15 +1,3 @@
-(* Require Import Reals List Rtrigo_def Coquelicot.Derive.
-Open Scope R.
-Definition putnam_2023_a1_solution : nat := 18.
-Theorem putnam_2023_a1
-    (f : nat -> R -> R := fun n (x : R) => 
-        let f_i i := cos (INR i * x) in 
-            let coeffs := map f_i (seq 1 n) in 
-                fold_right Rmult 1 coeffs
-    )
-    : gt putnam_2023_a1_solution 0 /\ Derive_n (f putnam_2023_a1_solution) 2 0 > 2023 /\ (forall n : nat, (gt n 0 /\ lt n putnam_2023_a1_solution) -> Derive_n (f n) 2 0 <= 2023).
-Proof. Admitted.  *)
-
 From mathcomp Require Import all_ssreflect ssrnum ssralg.
 From mathcomp Require Import reals trigo normedtype derive topology sequences.
 From mathcomp Require Import classical_sets.
