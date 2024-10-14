fca6ef3ba357c2e4e23575834524da2212fdd42a
Eric Wieser <efw@google.com>
2024-10-14 17:01:38 +0100

    Drop a useless assumption

--- lean4/src/putnam_2022_b1.lean
+++ lean4/src/putnam_2022_b1.lean
@@ -7,12 +7,9 @@ open Polynomial
 Suppose that $P(x)=a_1x+a_2x^2+\cdots+a_nx^n$ is a polynomial with integer coefficients, with $a_1$ odd. Suppose that $e^{P(x)}=b_0+b_1x+b_2x^2+\dots$ for all $x$. Prove that $b_k$ is nonzero for all $k \geq 0$.
 -/
 theorem putnam_2022_b1
-    (n : ℕ)
     (P : Polynomial ℤ)
     (b : ℕ → ℝ)
-    (npos : n ≥ 1)
     (Pconst : P.coeff 0 = 0)
-    (Pdegree : P.degree = n)
     (Podd : Odd (P.coeff 1))
     (hB : ∀ x : ℝ, HasSum (fun i => b i * x ^ i) (Real.exp (aeval x P))) :
     ∀ k : ℕ, b k ≠ 0 := by
