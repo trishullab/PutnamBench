27491bccc7f74bf3039a6612a916041fc8fec595
Eric Wieser <efw@google.com>
2024-10-21 11:19:38 +0000

    Use `⟪x, y⟫_ℝ` instead of `Matrix.dotProduct`

--- lean4/src/putnam_1998_a6.lean
+++ lean4/src/putnam_1998_a6.lean
@@ -2,6 +2,7 @@ import Mathlib
 open BigOperators
 
 open Set Function Metric
+open scoped InnerProductSpace
 
 /--
 Let $A, B, C$ denote distinct points with integer coordinates in $\mathbb R^2$. Prove that if
@@ -13,6 +14,6 @@ theorem putnam_1998_a6
 (hint : ∀ i : Fin 2, ∃ a b c : ℤ, A i = a ∧ B i = b ∧ C i = c)
 (htriangle : A ≠ B ∧ A ≠ C ∧ B ≠ C)
 (harea : (dist A B + dist B C) ^ 2 < 8 * (MeasureTheory.volume (convexHull ℝ {A, B, C})).toReal + 1)
-(threesquare : (EuclideanSpace ℝ (Fin 2)) → (EuclideanSpace ℝ (Fin 2)) → (EuclideanSpace ℝ (Fin 2)) → Prop := fun P Q R ↦ dist Q P = dist Q R ∧ Matrix.dotProduct (P - Q) (R - Q) = 0)
+(threesquare : EuclideanSpace ℝ (Fin 2) → EuclideanSpace ℝ (Fin 2) → EuclideanSpace ℝ (Fin 2) → Prop := fun P Q R ↦ dist Q P = dist Q R ∧ ⟪P - Q, R - Q⟫_ℝ = 0)
 : (threesquare A B C ∨ threesquare B C A ∨ threesquare C A B) :=
 sorry
