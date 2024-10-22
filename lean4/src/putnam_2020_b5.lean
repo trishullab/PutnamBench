1d20ba611f98416ed172882d1d9c10742ef7ca86
Oliver Nash <ddojsioc@google.com>
2024-10-22 16:08:34 +0000

    Fix 2020 B5
    The error was a simple typo: `<` instead of `=`.

--- lean4/src/putnam_2020_b5.lean
+++ lean4/src/putnam_2020_b5.lean
@@ -8,7 +8,7 @@ For $j \in \{1, 2, 3, 4\}$, let $z_j$ be a complex number with $|z_j| = 1$ and $
 -/
 theorem putnam_2020_b5
 (z : Fin 4 → ℂ)
-(hzle1 : ∀ n, ‖z n‖ < 1)
+(hzle1 : ∀ n, ‖z n‖ = 1)
 (hzne1 : ∀ n, z n ≠ 1)
 : 3 - z 0 - z 1 - z 2 - z 3 + (z 0) * (z 1) * (z 2) * (z 3) ≠ 0:=
 sorry
