e37a306070cef4dcd202e113410d486322ffce42
Oliver Nash <ddojsioc@google.com>
2024-10-22 16:16:50 +0000

    Fix 2020 A5
    The problem was that `(a * b : Fin 3 → ℝ)` first coerces `a` and `b`
    to vectors and then uses the multiplication on vectors (which is
    pointwise multiplication). Thus the group structure declared in `hGgrp`
    was completely ignored. This could have been fixed by writing
    `(↑(a * b) : Fin 3 → ℝ)` but I think it is much clearer to use an
    embedding and not depend upon arcane details Lean's coercion
    elaboration.

--- lean4/src/putnam_2010_a5.lean
+++ lean4/src/putnam_2010_a5.lean
@@ -1,12 +1,15 @@
 import Mathlib
 open BigOperators
 
+open scoped Matrix
+
 /--
 Let $G$ be a group, with operation $*$. Suppose that \begin{enumerate} \item[(i)] $G$ is a subset of $\mathbb{R}^3$ (but $*$ need not be related to addition of vectors); \item[(ii)] For each $\mathbf{a},\mathbf{b} \in G$, either $\mathbf{a}\times \mathbf{b} = \mathbf{a}*\mathbf{b}$ or $\mathbf{a}\times \mathbf{b} = 0$ (or both), where $\times$ is the usual cross product in $\mathbb{R}^3$. \end{enumerate} Prove that $\mathbf{a} \times \mathbf{b} = 0$ for all $\mathbf{a}, \mathbf{b} \in G$.
 -/
 theorem putnam_2010_a5
-(G : Set (Fin 3 → ℝ))
-(hGgrp : Group G)
-(hGcross : ∀ a b : G, crossProduct a b = (a * b : Fin 3 → ℝ) ∨ crossProduct (a : Fin 3 → ℝ) b = 0)
-: ∀ a b : G, crossProduct (a : Fin 3 → ℝ) b = 0 :=
-sorry
+    (G : Type*) [Group G]
+    (i : G ↪ (Fin 3 → ℝ))
+    (h : ∀ a b, (i a) ×₃ (i b) = i (a * b) ∨ (i a) ×₃ (i b) = 0)
+    (a b : G) :
+    (i a) ×₃ (i b) = 0 :=
+  sorry
