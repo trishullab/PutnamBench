cc97218ac25b559572f968b2be913746f82b38ef
Paul Lezeau <lezeau@google.com>
2025-02-26 11:11:25 +0000

    Fix build errors

--- lean4/src/putnam_2024_a5.lean
+++ lean4/src/putnam_2024_a5.lean
@@ -11,9 +11,9 @@ Consider a circle $\Omega$ with radius $9$ and center at the origin $(0, 0)$ and
 on $\Omega$. Which value(s) of $r$ minimize the probability that the chord $\overline{PQ}$ intersects $\Delta$?
 -/
 theorem putnam_2024_a5
-    (IsIntersecting : ℝ → EuclideanSpace (Fin 2) ℝ → EuclideanSpace (Fin 2) ℝ → Prop)
+    (IsIntersecting : ℝ → EuclideanSpace ℝ (Fin 2) → EuclideanSpace ℝ (Fin 2) → Prop)
     (IsIntersecting_def : ∀ r P Q,
-      IsIntersecting r P Q ↔ (line[ℝ,P,Q] : Set (EuclideanSpace (Fin 2) ℝ))
+      IsIntersecting r P Q ↔ (line[ℝ,P,Q] : Set (EuclideanSpace ℝ (Fin 2)))
       ∩ Metric.sphere ((WithLp.equiv _ _).symm ![r, 0]) 1 ≠ ∅)
     (ProbIntersect : ℝ → ℝ≥0∞)
     (ProbIntersect_def : ∀ r,
