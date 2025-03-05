7631411955c3509ff4f4c6b18f9779d51916e7f5
Paul Lezeau <lezeau@google.com>
2025-03-05 16:41:19 +0000

    Fix statement docstring

--- lean4/src/putnam_2010_a3.lean
+++ lean4/src/putnam_2010_a3.lean
@@ -1,12 +1,6 @@
 import Mathlib
 
-/--
-Suppose that the function `h : ℝ^2 → ℝ` has continuous
-partial derivatives and satisfies the equation `h(x, y) = a ∂h/∂x(x, y) +b ∂h/∂y(x, y)`
-for some constants `a,b`. Prove that if there is a constant
-`M` such that `|h(x, y)| ≤ M` for all `(x, y) ∈ ℝ^2`, then `h` is
-identically zero.
--/
+/--Suppose that the function $h : \\mathbb{R}^2 → \\mathbb{R}$ has continuous partial derivatives and satisfies the equation $h(x, y) = a \frac{\\partial h}{\\partial x}(x, y) +b \frac{\\partial h}{\\partial y}(x, y)$ for some constants $a, b$. Prove that if there is a constant $M$ such that $|h(x, y)| ≤ M$ for all $(x, y) ∈ \\mathbb{R}^2$, then $h$ is identically zero.-/
 theorem putnam_2010_a3
     (h : ℝ × ℝ → ℝ)
     (a b M : ℝ)
