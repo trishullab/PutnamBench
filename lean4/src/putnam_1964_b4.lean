18c52ca868fec8bf9ca169c71ca5bc9fda9f296f
Paul Lezeau <lezeau@google.com>
2025-03-25 17:29:02 +0000

    Add missing non-zero condition

--- lean4/src/putnam_1964_b4.lean
+++ lean4/src/putnam_1964_b4.lean
@@ -17,6 +17,8 @@ theorem putnam_1964_b4
     (v : Fin n → EuclideanSpace ℝ (Fin 3))
     -- A great circle `C` is the intersection of a plane `{x | x · v = 0}` with `S`
     (hv : ∀ i, C i = Metric.sphere 0 1 ∩ {x : EuclideanSpace ℝ (Fin 3) | ⟪v i, x⟫_ℝ = 0 })
+    --all the `v_i`'s are non-zero
+    (hv' : ∀ i, v i ≠ 0)
     -- The circles in `C` are in general position
     (hT₂ : ∀ᵉ (x ∈ Metric.sphere 0 1) (y ∈ Metric.sphere 0 1),
       (Finset.univ.filter (fun i => {x, y} ⊆ (C i))).card ≤ 2)
@@ -28,5 +30,5 @@ theorem putnam_1964_b4
       R ⊆ Metric.sphere 0 1 ∧ R.Nonempty ∧ ∃ sign : Fin n → SignType,
       (∀ i, sign i ≠ 0) ∧
       R = {x : EuclideanSpace ℝ (Fin 3) | ∀ i, signHom ⟪v i, x⟫_ℝ = sign i}) :
-    {R | IsRegion R}.ncard = putnam_1964_b4_solution n :=
+    {R | IsRegion R}.ncard = putnam_1964_b4_solution (n+1) :=
   sorry
