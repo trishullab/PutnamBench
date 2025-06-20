4587df7cce7325ce41f7f223dbd5ff22a438c57f
Paul Lezeau <lezeau@google.com>
2025-06-20 14:18:33 +0000

    Fixes

--- lean4/src/putnam_2022_a4.lean
+++ lean4/src/putnam_2022_a4.lean
@@ -6,9 +6,7 @@ abbrev putnam_2022_a4_solution : ℝ := sorry
 -- 2*Real.exp (-1/2) - 3
 
 /--
-Suppose $X_1, X_2, ...$ real numbers between 0 and 1 that are chosen independently and uniformly at random.
-Let $S = \sum_{i=1}^k X_i / 2^i $ where $k$ is the least positive integer such that $X_k < X_{k+1}$ or $k = \infty$
-if there is no such integer. Find the expected value of $S$
+Suppose $X_1, X_2, ...$ real numbers between 0 and 1 that are chosen independently and uniformly at random. Let $S = \sum_{i=1}^k X_i / 2^i $ where $k$ is the least positive integer such that $X_k < X_{k+1}$ or $k = \infty$ if there is no such integer. Find the expected value of $S$
 -/
 theorem putnam_2022_a4
     {Ω : Type*} [MeasureSpace Ω]
@@ -23,8 +21,8 @@ theorem putnam_2022_a4
     Otherwise, this is `sSup { (Set.Iic m : Set ℕ) | (m : ℕ) } = Set.univ`
     -/
     (hk : ∀ ω, k ω = sSup { s : Set ℕ |
-      ∃ m, s = Set.Iic m ∧ ∀ l ∈ s, X (l+1) ω ≥ X (l+1) ω })
+      ∃ m, s = Set.Iic m ∧ ∀ l ∈ s, X l ω < X (l + 1) ω })
     (S : Ω → ℝ)
-    (hS : ∀ ω, S ω = ∑' (i : k ω), (X i ω) / (2^(i.val+1))) :
+    (hS : ∀ ω, S ω = ∑' (i : k ω), (X i ω) / (2 ^ (i.val + 1))) :
     ∫ ω, S ω ∂(ℙ : Measure Ω) = putnam_2022_a4_solution := by
   sorry
