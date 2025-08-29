ba4d84a828f81fa6ec3520133c43a7383bdae171
Eric Wieser <efw@google.com>
2025-08-29 08:28:11 +0000

    Update to Lean 4.21.0

--- lean4/lakefile.lean
+++ lean4/lakefile.lean
@@ -6,7 +6,7 @@ package «putnam» where
   leanOptions := #[
     ⟨`autoImplicit, false⟩
   ]
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.20.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.21.0"
 
 @[default_target]
 lean_lib «putnam» where
