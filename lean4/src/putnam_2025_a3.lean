38460df0e701c3f5fb9aab6cc5b065b04b72b41c
Jujian Zhang <jujian@axiommath.ai>
2025-12-09 13:39:53 +0000

    move docs string to theorem putnam_2025_a3

--- lean4/src/putnam_2025_a3.lean
+++ lean4/src/putnam_2025_a3.lean
@@ -2,14 +2,6 @@ import Mathlib
 
 open Finset Function
 
-/--
-Alice and Bob play a game with a string of $n$ digits, each of which is restricted
-to be 0, 1, or 2. Initially all the digits are 0. A legal move is to add or subtract 1
-from one digit to create a new string that has not appeared before. A player with no
-legal move loses, and the other player wins. Alice goes first, and the players alternate
-moves. For each $n \ge 1$, determine which player has a strategy that guarantees winning.
--/
-
 abbrev GameString (n : ℕ) := Fin n → Fin 3
 
 abbrev initialState (n : ℕ) : GameString n := fun _ => 0
@@ -34,6 +26,13 @@ abbrev AliceHasWinningStrategy (n : ℕ) : Prop := HasWinningStrategy n []
 abbrev putnam_2025_a3_solution : ℕ → Prop := sorry
 -- fun _ => False
 
+/--
+Alice and Bob play a game with a string of $n$ digits, each of which is restricted
+to be 0, 1, or 2. Initially all the digits are 0. A legal move is to add or subtract 1
+from one digit to create a new string that has not appeared before. A player with no
+legal move loses, and the other player wins. Alice goes first, and the players alternate
+moves. For each $n \ge 1$, determine which player has a strategy that guarantees winning.
+-/
 theorem putnam_2025_a3 (n : ℕ) (hn : 1 ≤ n) :
     putnam_2025_a3_solution n ↔ AliceHasWinningStrategy n := by
   sorry
