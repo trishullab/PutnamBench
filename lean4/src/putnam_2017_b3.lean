d8df777a6b5e057be34721e846a1e2f3055cfb28
svarambally-mistral <sumanth.varambally@mistral.ai>
2026-07-27 12:48:25 -0700

    Fix 2017 B3 statement

--- lean4/src/putnam_2017_b3.lean
+++ lean4/src/putnam_2017_b3.lean
@@ -10,5 +10,5 @@ theorem putnam_2017_b3
 (c : ℕ → ℝ)
 (hc : ∀ n, c n = 0 ∨ c n = 1)
 (hf : ∀ x, f x = ∑' n : ℕ, (c n) * x^n)
-: f (2/3) = 3/2 → Irrational (f 1/2) :=
+: f (2/3) = 3/2 → Irrational (f (1/2)) :=
 sorry
